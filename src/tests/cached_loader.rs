d64fdc24d6764a7be46b98c0ffd70b31aaa732a3
tyranron <tyranron@gmail.com>
2019-06-17 17:06:38 +0300

    Upd tests

--- src/tests/cached_loader.rs
+++ src/tests/cached_loader.rs
@@ -10,44 +10,41 @@ fn assert_kinds() {
     fn _assert_sync<T: Sync>() {}
     fn _assert_clone<T: Clone>() {}
     _assert_send::<
-        cached::Loader<u32, u32, u32, Batcher, BTreeMap<u32, Result<u32, LoadError<u32>>>>,
+        cached::Loader<i32, i32, (), Batcher, BTreeMap<i32, cached::Item<i32, i32, (), Batcher>>>,
     >();
     _assert_sync::<
-        cached::Loader<u32, u32, u32, Batcher, BTreeMap<u32, Result<u32, LoadError<u32>>>>,
+        cached::Loader<i32, i32, (), Batcher, BTreeMap<i32, cached::Item<i32, i32, (), Batcher>>>,
     >();
     _assert_clone::<
-        cached::Loader<u32, u32, u32, Batcher, BTreeMap<u32, Result<u32, LoadError<u32>>>>,
+        cached::Loader<i32, i32, (), Batcher, BTreeMap<i32, cached::Item<i32, i32, (), Batcher>>>,
     >();
 }
 
 #[test]
 fn smoke() {
-    let mut rt = executor::LocalPool::new();
-
     let loader = Loader::new(Batcher::new(2)).cached();
 
     let v = future::try_join3(loader.load(1), loader.load(2), loader.load(3));
-    assert_eq!((10, 20, 30), rt.run_until(v).unwrap());
+    assert_eq!((10, 20, 30), executor::block_on(v).unwrap());
 
     let v = loader.load_many(vec![10, 20, 30]);
-    assert_eq!(vec![100, 200, 300], rt.run_until(v).unwrap());
+    assert_eq!(vec![100, 200, 300], executor::block_on(v).unwrap());
 
     let loader_ref = &loader;
     {
         let v = future::try_join(loader_ref.load(1), loader_ref.load(2));
-        assert_eq!((10, 20), rt.run_until(v).unwrap());
+        assert_eq!((10, 20), executor::block_on(v).unwrap());
     }
     {
         let v1 = loader_ref.load(3).and_then(|v| loader_ref.load(v));
         let v2 = loader_ref.load(4).and_then(|v| loader_ref.load(v));
-        assert_eq!((300, 400), rt.run_until(future::try_join(v1, v2)).unwrap());
+        let all = future::try_join(v1, v2);
+        assert_eq!((300, 400), executor::block_on(all).unwrap());
     }
 }
 
 #[test]
 fn drop_loader() {
-    let mut rt = executor::LocalPool::new();
-
     let all = {
         let loader = Loader::new(Batcher::new(10)).cached();
         let v1 = loader.load(1);
@@ -55,32 +52,27 @@ fn drop_loader() {
         drop(loader);
         future::try_join(v1, v2)
     };
-    assert_eq!((10, 20), rt.run_until(all).unwrap());
+    assert_eq!((10, 20), executor::block_on(all).unwrap());
 }
 
 #[test]
 fn dispatch_partial_batch() {
-    let mut rt = executor::LocalPool::new();
-
     let loader = Loader::new(Batcher::new(10)).cached();
     let all = future::try_join(loader.load(1), loader.load(2));
-    assert_eq!((10, 20), rt.run_until(all).unwrap());
+    assert_eq!((10, 20), executor::block_on(all).unwrap());
 }
 
 #[test]
 fn nested_load() {
-    let mut rt = executor::LocalPool::new();
-
     let loader = Loader::new(Batcher::new(2)).cached();
     let v1 = loader.load(3).and_then(|v| loader.load(v));
     let v2 = loader.load(4).and_then(|v| loader.load(v));
-    assert_eq!((300, 400), rt.run_until(future::try_join(v1, v2)).unwrap());
+    let all = future::try_join(v1, v2);
+    assert_eq!((300, 400), executor::block_on(all).unwrap());
 }
 
 #[test]
 fn nested_load_many() {
-    let mut rt = executor::LocalPool::new();
-
     let loader = Loader::new(Batcher::new(2)).cached();
     let v1 = loader
         .load(3)
@@ -89,55 +81,52 @@ fn nested_load_many() {
         .load(4)
         .and_then(|v| loader.load_many(vec![v, v + 1, v + 2]));
     let expected = (vec![300, 310, 320], vec![400, 410, 420]);
-    assert_eq!(expected, rt.run_until(future::try_join(v1, v2)).unwrap());
+    let all = future::try_join(v1, v2);
+    assert_eq!(expected, executor::block_on(all).unwrap());
 }
 
 #[test]
 fn test_batch_fn_error() {
-    let mut rt = executor::LocalPool::new();
-
     let loader = Loader::<i32, i32, MyError, BadBatcher>::new(BadBatcher).cached();
-    let v1 = rt.run_until(loader.load(1));
+    let v1 = executor::block_on(loader.load(1));
     assert_eq!(LoadError::BatchFn(MyError::Unknown), v1.err().unwrap());
 }
 
 #[test]
 fn test_result_val() {
-    let mut rt = executor::LocalPool::new();
-
     let loader =
         Loader::<i32, Result<i32, ValueError>, MyError, BadBatcher>::new(BadBatcher).cached();
-    let v1 = rt.run_until(loader.load_many(vec![1, 2]));
+    let v1 = executor::block_on(loader.load_many(vec![1, 2]));
     assert_eq!(vec![Err(ValueError::NotEven), Ok(20)], v1.unwrap());
 }
 
 #[test]
 fn test_batch_call_seq() {
-    let mut rt = executor::LocalPool::new();
-
     // batch size = 2, value will be (batch_fn call seq, v * 10)
     let loader = Loader::<i32, (usize, i32), (), _>::new(Batcher::new(2)).cached();
+
     let v1 = loader.load(1);
     let v2 = loader.load(2);
-    let v3 = loader.load(3);
-    let v4 = loader.load(4);
-    let v5 = loader.load(1);
-    let v6 = loader.load(2);
-
     //v1 and v2 should be in first batch
     assert_eq!(
         ((1, 10), (1, 20)),
-        rt.run_until(future::try_join(v1, v2)).unwrap(),
+        executor::block_on(future::try_join(v1, v2)).unwrap(),
     );
+
+    let v3 = loader.load(3);
+    let v4 = loader.load(4);
     //v3 and v4 should be in second batch
     assert_eq!(
         ((2, 30), (2, 40)),
-        rt.run_until(future::try_join(v3, v4)).unwrap(),
+        executor::block_on(future::try_join(v3, v4)).unwrap(),
     );
+
+    let v5 = loader.load(1);
+    let v6 = loader.load(2);
     //v5 and v6 should be using cache of first batch
     assert_eq!(
         ((1, 10), (1, 20)),
-        rt.run_until(future::try_join(v5, v6)).unwrap(),
+        executor::block_on(future::try_join(v5, v6)).unwrap(),
     );
 }
 
@@ -147,16 +136,14 @@ fn pass_to_thread() {
 
     let l = loader.clone();
     let h1 = thread::spawn(move || {
-        let mut rt = executor::LocalPool::new();
         let all = future::try_join(l.load(1), l.load(2));
-        assert_eq!((10, 20), rt.run_until(all).unwrap());
+        assert_eq!((10, 20), executor::block_on(all).unwrap());
     });
 
     let l2 = loader.clone();
     let h2 = thread::spawn(move || {
-        let mut rt = executor::LocalPool::new();
         let all = future::try_join(l2.load(1), l2.load(2));
-        assert_eq!((10, 20), rt.run_until(all).unwrap());
+        assert_eq!((10, 20), executor::block_on(all).unwrap());
     });
 
     let _ = h1.join();
@@ -182,32 +169,28 @@ fn test_run_by_threadpool() {
 
 #[test]
 fn test_clear() {
-    let mut rt = executor::LocalPool::new();
-
     // batch size = 2, value will be (batch_fn call seq, v * 10)
     let loader = Loader::<i32, (usize, i32), (), _>::new(Batcher::new(2)).cached();
     let v1 = loader.load(1);
     let v2 = loader.load(1);
     assert_eq!(
         ((1, 10), (1, 10)),
-        rt.run_until(future::try_join(v1, v2)).unwrap(),
+        executor::block_on(future::try_join(v1, v2)).unwrap(),
     );
 
     loader.remove(&1);
-    assert_eq!((2, 10), rt.run_until(loader.load(1)).unwrap());
+    assert_eq!((2, 10), executor::block_on(loader.load(1)).unwrap());
 }
 
 #[test]
 fn test_clear_all() {
-    let mut rt = executor::LocalPool::new();
-
     // batch size = 2, value will be (batch_fn call seq, v * 10)
     let loader = Loader::<i32, (usize, i32), (), _>::new(Batcher::new(2)).cached();
     let v1 = loader.load(1);
     let v2 = loader.load(2);
     assert_eq!(
         ((1, 10), (1, 20)),
-        rt.run_until(future::try_join(v1, v2)).unwrap(),
+        executor::block_on(future::try_join(v1, v2)).unwrap(),
     );
 
     loader.clear();
@@ -215,31 +198,27 @@ fn test_clear_all() {
     let v4 = loader.load(2);
     assert_eq!(
         ((2, 10), (2, 20)),
-        rt.run_until(future::try_join(v3, v4)).unwrap(),
+        executor::block_on(future::try_join(v3, v4)).unwrap(),
     );
 }
 
 #[test]
 fn test_prime() {
-    let mut rt = executor::LocalPool::new();
-
     // batch size = 1, value will be (batch_fn call seq, v * 10)
     let loader = Loader::<i32, (usize, i32), (), _>::new(Batcher::new(1)).cached();
     loader.prime(1, (0, 101));
     let v1 = loader.load(1);
     let v2 = loader.load(2);
-    assert_eq!((0, 101), rt.run_until(v1).unwrap());
-    assert_eq!((1, 20), rt.run_until(v2).unwrap());
+    assert_eq!((0, 101), executor::block_on(v1).unwrap());
+    assert_eq!((1, 20), executor::block_on(v2).unwrap());
 
     loader.prime(2, (0, 201)); // should have no effect as key 2 are loaded already
     let v3 = loader.load(2);
-    assert_eq!((1, 20), rt.run_until(v3).unwrap());
+    assert_eq!((1, 20), executor::block_on(v3).unwrap());
 }
 
 #[test]
 fn test_custom_cache() {
-    let mut rt = executor::LocalPool::new();
-
     // batch size = 2, value will be (batch_fn call seq, v * 10)
     let loader =
         Loader::<i32, (usize, i32), (), _>::new(Batcher::new(2)).with_cache(MyCache::new());
@@ -247,7 +226,7 @@ fn test_custom_cache() {
     let v2 = loader.load(2);
     assert_eq!(
         ((1, 10), (1, 20)),
-        rt.run_until(future::try_join(v1, v2)).unwrap(),
+        executor::block_on(future::try_join(v1, v2)).unwrap(),
     );
 
     loader.clear();
@@ -255,6 +234,6 @@ fn test_custom_cache() {
     let v4 = loader.load(2);
     assert_eq!(
         ((2, 10), (2, 20)),
-        rt.run_until(future::try_join(v3, v4)).unwrap(),
+        executor::block_on(future::try_join(v3, v4)).unwrap(),
     );
 }
--- src/tests/non_cached_loader.rs
+++ src/tests/non_cached_loader.rs
@@ -16,32 +16,29 @@ fn assert_kinds() {
 
 #[test]
 fn smoke() {
-    let mut rt = executor::LocalPool::new();
-
     let loader = Loader::new(Batcher::new(2));
 
     let v = future::try_join3(loader.load(1), loader.load(2), loader.load(3));
-    assert_eq!((10, 20, 30), rt.run_until(v).unwrap());
+    assert_eq!((10, 20, 30), executor::block_on(v).unwrap());
 
     let v = loader.load_many(vec![10, 20, 30]);
-    assert_eq!(vec![100, 200, 300], rt.run_until(v).unwrap());
+    assert_eq!(vec![100, 200, 300], executor::block_on(v).unwrap());
 
     let loader_ref = &loader;
     {
         let v = future::try_join(loader_ref.load(1), loader_ref.load(2));
-        assert_eq!((10, 20), rt.run_until(v).unwrap());
+        assert_eq!((10, 20), executor::block_on(v).unwrap());
     }
     {
         let v1 = loader_ref.load(3).and_then(|v| loader_ref.load(v));
         let v2 = loader_ref.load(4).and_then(|v| loader_ref.load(v));
-        assert_eq!((300, 400), rt.run_until(future::try_join(v1, v2)).unwrap());
+        let all = future::try_join(v1, v2);
+        assert_eq!((300, 400), executor::block_on(all).unwrap());
     }
 }
 
 #[test]
 fn drop_loader() {
-    let mut rt = executor::LocalPool::new();
-
     let all = {
         let loader = Loader::new(Batcher::new(10));
         let v1 = loader.load(1);
@@ -49,33 +46,27 @@ fn drop_loader() {
         drop(loader);
         future::try_join(v1, v2)
     };
-    assert_eq!((10, 20), rt.run_until(all).unwrap());
+    assert_eq!((10, 20), executor::block_on(all).unwrap());
 }
 
 #[test]
 fn dispatch_partial_batch() {
-    let mut rt = executor::LocalPool::new();
-
     let loader = Loader::new(Batcher::new(10));
     let all = future::try_join(loader.load(1), loader.load(2));
-
-    assert_eq!((10, 20), rt.run_until(all).unwrap());
+    assert_eq!((10, 20), executor::block_on(all).unwrap());
 }
 
 #[test]
 fn nested_load() {
-    let mut rt = executor::LocalPool::new();
-
     let loader = Loader::new(Batcher::new(2));
     let v1 = loader.load(3).and_then(|v| loader.load(v));
     let v2 = loader.load(4).and_then(|v| loader.load(v));
-    assert_eq!((300, 400), rt.run_until(future::try_join(v1, v2)).unwrap());
+    let all = future::try_join(v1, v2);
+    assert_eq!((300, 400), executor::block_on(all).unwrap());
 }
 
 #[test]
 fn nested_load_many() {
-    let mut rt = executor::LocalPool::new();
-
     let loader = Loader::new(Batcher::new(2));
     let v1 = loader
         .load(3)
@@ -84,54 +75,49 @@ fn nested_load_many() {
         .load(4)
         .and_then(|v| loader.load_many(vec![v, v + 1, v + 2]));
     let expected = (vec![300, 310, 320], vec![400, 410, 420]);
-    assert_eq!(expected, rt.run_until(future::try_join(v1, v2)).unwrap());
+    let all = future::try_join(v1, v2);
+    assert_eq!(expected, executor::block_on(all).unwrap());
 }
 
 #[test]
 fn test_batch_fn_error() {
-    let mut rt = executor::LocalPool::new();
-
     let loader = Loader::<i32, i32, MyError, BadBatcher>::new(BadBatcher);
-    let v1 = rt.run_until(loader.load(1));
+    let v1 = executor::block_on(loader.load(1));
     assert_eq!(LoadError::BatchFn(MyError::Unknown), v1.err().unwrap());
 }
 
 #[test]
 fn test_result_val() {
-    let mut rt = executor::LocalPool::new();
-
     let loader = Loader::<i32, Result<i32, ValueError>, MyError, BadBatcher>::new(BadBatcher);
-    let v1 = rt.run_until(loader.load_many(vec![1, 2]));
+    let v1 = executor::block_on(loader.load_many(vec![1, 2]));
     assert_eq!(vec![Err(ValueError::NotEven), Ok(20)], v1.unwrap());
 }
 
 #[test]
 fn test_batch_call_seq() {
-    let mut rt = executor::LocalPool::new();
-
     // batch size = 2, value will be (batch_fn call seq, v * 10)
     let loader = Loader::<i32, (usize, i32), (), Batcher>::new(Batcher::new(2));
+
     let v1 = loader.load(1);
     let v2 = loader.load(2);
-    let v3 = loader.load(3);
-    let v4 = loader.load(4);
-    let v5 = loader.load(1);
-    let v6 = loader.load(2);
-
-    //v1 and v2 should be in first batch
     assert_eq!(
         ((1, 10), (1, 20)),
-        rt.run_until(future::try_join(v1, v2)).unwrap(),
+        executor::block_on(future::try_join(v1, v2)).unwrap(),
     );
-    //v3 and v4 should be in second batch
+
+    let v3 = loader.load(3);
+    let v4 = loader.load(4);
     assert_eq!(
         ((2, 30), (2, 40)),
-        rt.run_until(future::try_join(v3, v4)).unwrap(),
+        executor::block_on(future::try_join(v3, v4)).unwrap(),
     );
+
+    let v5 = loader.load(1);
+    let v6 = loader.load(2);
     //v5 and v6 should be be in third batch
     assert_eq!(
         ((3, 10), (3, 20)),
-        rt.run_until(future::try_join(v5, v6)).unwrap(),
+        executor::block_on(future::try_join(v5, v6)).unwrap(),
     );
 }
 
@@ -141,16 +127,14 @@ fn pass_to_thread() {
 
     let l = loader.clone();
     let h1 = thread::spawn(move || {
-        let mut rt = executor::LocalPool::new();
         let all = future::try_join(l.load(1), l.load(2));
-        assert_eq!((10, 20), rt.run_until(all).unwrap());
+        assert_eq!((10, 20), executor::block_on(all).unwrap());
     });
 
     let l2 = loader.clone();
     let h2 = thread::spawn(move || {
-        let mut rt = executor::LocalPool::new();
         let all = future::try_join(l2.load(1), l2.load(2));
-        assert_eq!((10, 20), rt.run_until(all).unwrap());
+        assert_eq!((10, 20), executor::block_on(all).unwrap());
     });
 
     let _ = h1.join();
@@ -176,12 +160,10 @@ fn test_run_by_threadpool() {
 
 #[test]
 fn test_values_length() {
-    let mut rt = executor::LocalPool::new();
-
     let loader = Loader::<i32, (), (), BadBatcher>::new(BadBatcher);
     let v1 = loader.load(1);
     let v2 = loader.load(2);
-    let (r1, r2) = rt.run_until(future::join(v1, v2));
+    let (r1, r2) = executor::block_on(future::join(v1, v2));
     assert_eq!(
         LoadError::UnequalKeyValueSize {
             key_count: 2,
